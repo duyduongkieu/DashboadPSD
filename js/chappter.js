904ee8dc00d49991c453f5274d13cf901115cb57
duong <duyduong240798@gmail.com>
2018-11-27 09:50:18 +0700

    done

--- js/chappter.js
+++ js/chappter.js
@@ -1,39 +1,38 @@
 var options = {
-  chart: {
-    height: 200,
-    type: 'radialBar'
-  },
+	chart: {
+		height: 200,
+		type: 'radialBar',
+	},
+	series: [50],
+	colors: ['#e76c90'],
+	plotOptions: {
+		radialBar: {
+			hollow: {
+				margin: 15,
+				size: '70%',
+			},
 
-  series: [50],
-  colors: ['#e76c90'],
-  plotOptions: {
-    radialBar: {
-      hollow: {
-        margin: 15,
-        size: '70%'
-      },
+			dataLabels: {
+				showOn: 'always',
+				name: {
+					offsetY: -10,
+					show: true,
+					color: '#888',
+					fontSize: '13px',
+				},
+				value: {
+					color: '#111',
+					fontSize: '30px',
+					show: true,
+				},
+			},
+		},
+	},
 
-      dataLabels: {
-        showOn: 'always',
-        name: {
-          offsetY: -10,
-          show: true,
-          color: '#888',
-          fontSize: '13px'
-        },
-        value: {
-          color: '#111',
-          fontSize: '30px',
-          show: true
-        }
-      }
-    }
-  },
-
-  stroke: {
-    lineCap: 'round'
-  },
-  labels: ['']
+	stroke: {
+		lineCap: 'round',
+	},
+	labels: [''],
 };
 
 var chart = new ApexCharts(document.querySelector('#chart1'), options);
@@ -41,41 +40,41 @@ var chart = new ApexCharts(document.querySelector('#chart1'), options);
 chart.render();
 // ....................
 var options = {
-  chart: {
-    height: 200,
-    type: 'radialBar'
-  },
+	chart: {
+		height: 200,
+		type: 'radialBar',
+	},
 
-  series: [50],
-  colors: ['#fff'],
-  plotOptions: {
-    radialBar: {
-      hollow: {
-        margin: 15,
-        size: '70%'
-      },
+	series: [50],
+	colors: ['#fff'],
+	plotOptions: {
+		radialBar: {
+			hollow: {
+				margin: 15,
+				size: '70%',
+			},
 
-      dataLabels: {
-        showOn: 'always',
-        name: {
-          offsetY: -10,
-          show: true,
-          color: '#888',
-          fontSize: '13px'
-        },
-        value: {
-          color: '#111',
-          fontSize: '30px',
-          show: true
-        }
-      }
-    }
-  },
+			dataLabels: {
+				showOn: 'always',
+				name: {
+					offsetY: -10,
+					show: true,
+					color: '#888',
+					fontSize: '13px',
+				},
+				value: {
+					color: '#fff',
+					fontSize: '30px',
+					show: true,
+				},
+			},
+		},
+	},
 
-  stroke: {
-    lineCap: 'round'
-  },
-  labels: ['']
+	stroke: {
+		lineCap: 'round',
+	},
+	labels: [''],
 };
 
 var chart = new ApexCharts(document.querySelector('#chart2'), options);
@@ -84,24 +83,144 @@ chart.render();
 // ...............
 
 var options = {
-  chart: {
-    type: 'heatmap',
-    background: '#red',
-    foreColor: '#red',
-    height: 400
-  },
-  series: [
-    {
-      name: 'sales',
-      data: [30, 40, 45, 50, 49, 60, 70, 91, 125]
-    }
-  ],
-  xaxis: {
-    categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999]
-  }
+	chart: {
+		height: 480,
+		type: 'bar',
+	},
+	plotOptions: {
+		bar: {
+			dataLabels: {
+				position: 'top', // top, center, bottom
+			},
+		},
+	},
+	dataLabels: {
+		enabled: false,
+		formatter: function(val) {
+			return val + '%';
+		},
+		offsetY: 0,
+		style: {
+			fontSize: '12px',
+			colors: ['#0000'],
+		},
+	},
+	series: [
+		{
+			name: 'Inflation',
+			data: [75, 60, 60, 35, 75, 60, 60, 35, 75, 60, 60, 35],
+		},
+	],
+	xaxis: {
+		categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+		position: 'bottom',
+		labels: {
+			offsetY: 0,
+		},
+		axisBorder: {
+			show: false,
+		},
+		axisTicks: {
+			show: false,
+		},
+		crosshairs: {
+			fill: {
+				type: 'gradient',
+				gradient: {
+					colorFrom: '#D8E3F0',
+					colorTo: '#BED1E6',
+					stops: [0, 100],
+					opacityFrom: 0.4,
+					opacityTo: 0.5,
+				},
+			},
+		},
+		tooltip: {
+			enabled: true,
+			offsetY: -35,
+		},
+	},
+	fill: {
+		gradient: {
+			enabled: false,
+			shade: 'light',
+			type: 'horizontal',
+			shadeIntensity: 0.25,
+			gradientToColors: undefined,
+			inverseColors: true,
+			opacityFrom: 1,
+			opacityTo: 1,
+			stops: [50, 0, 100, 100],
+		},
+	},
+	yaxis: {
+		axisBorder: {
+			show: false,
+		},
+		axisTicks: {
+			show: false,
+		},
+		labels: {
+			show: false,
+			formatter: function(val) {
+				return val + '%';
+			},
+		},
+	},
+	// title: {
+	// 	text: '',
+	// 	floating: true,
+	// 	offsetY: 320,
+	// 	align: 'center',
+	// 	style: {
+	// 		color: '#444',
+	// 	},
+	// },
 };
 
 var chart = new ApexCharts(document.querySelector('#chart3'), options);
 
 chart.render();
-// ...
+
+// .....
+var options = {
+	chart: {
+		height: 160,
+		type: 'radialBar',
+	},
+	series: [65],
+	colors: ['#06a0bd'],
+	plotOptions: {
+		radialBar: {
+			hollow: {
+				margin: 15,
+				size: '70%',
+			},
+
+			dataLabels: {
+				showOn: 'always',
+				name: {
+					offsetY: -10,
+					show: true,
+					color: '#888',
+					fontSize: '13px',
+				},
+				value: {
+					color: '#111',
+					fontSize: '30px',
+					show: true,
+				},
+			},
+		},
+	},
+
+	stroke: {
+		lineCap: 'round',
+	},
+	labels: [''],
+};
+
+var chart = new ApexCharts(document.querySelector('#chart4'), options);
+
+chart.render();
+// ......
--- js/main.js
+++ js/main.js
@@ -1,17 +1,27 @@
 $(document).ready(function() {
-  $('.icon1').on('click', function() {
-    var x = $(this);
-    $(x).toggleClass('icon');
+	$('.icon1').on('click', function() {
+		var x = $(this);
+		x.toggleClass('icon');
 
-    $(x)
-      .find('.arrowUp')
-      .slideToggle();
+		x.find('.arrowUp').slideToggle();
 
-    $(x)
-      .find('.arrowDown')
-      .slideToggle();
-    $(x)
-      .find('.menu-toggle')
-      .slideToggle();
-  });
+		x.find('.arrowDown').slideToggle();
+		x.find('.menu-toggle').slideToggle();
+	});
+	// .................
+	$('.pageBtn').on('click', function() {
+		event.preventDefault();
+	});
+	if (window.matchMedia('(min-width: 768px)').matches) {
+	} else {
+		/* the viewport is less than 400 pixels wide */
+	}
+	// ................
+	$('.icon-top-left').on('click', function() {
+		$('.body-left').css({ display: 'block', width: '35%' });
+		console.log('a');
+	});
+	$('.close-left').on('click', function() {
+		$('.body-left').css({ width: '0' });
+	});
 });
